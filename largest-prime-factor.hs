abee3f825f35daa2512daef8cf82a1a5692dc462
Berint Moffett <berint.moffett@gmail.com>
2024-05-19 01:52:01 -0600

    remove redundant helper functions

--- largest-prime-factor.hs
+++ largest-prime-factor.hs
@@ -1,19 +1,13 @@
-sqrtInt :: Integer -> Integer
-sqrtInt a = ceiling $ sqrt $ fromIntegral a
-
 possibleFactors :: Integer -> [Integer]
-possibleFactors a = filter (\x -> x /= a && x /= 1) [1..sqrtInt a]
-
-isDivisible :: Integer -> Integer -> Bool
-isDivisible a b = mod a b == 0
+possibleFactors a = filter (\x -> x /= a && x /= 1) [1..ceiling $ sqrt $ fromIntegral a]
 
 isPrime :: Integer -> Bool
 isPrime a = case possibleFactors a of
     [] -> True
-    _  -> and [not $ isDivisible a x | x <- possibleFactors a]
+    _  -> and [mod a x /= 0 | x <- possibleFactors a]
 
 getPrimeFactors :: Integer -> [Integer]
-getPrimeFactors a = filter (\x -> isDivisible a x && isPrime x) $ possibleFactors a
+getPrimeFactors a = filter (\x -> mod a x == 0 && isPrime x) $ possibleFactors a
 
 main :: IO ()
 main = do
